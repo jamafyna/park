ffbbf1dbd08d6c827c6a92d4fe481cc01f5c26f4
Jana Novotna <janka.novot@seznam.cz>
2015-07-29 13:21:45 +0200

    snaha o rozsirovani grafiky

---
+++ SpecialEffects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunaparkGame
+{
+    public class SpecialEffects:IActionable
+    {
+        public int propagate=0;
+        public int research=0;
+        public void Action() { }
+
+    }
+}
